4664c8cd59db5aea91d1d219343fdb2ada629161
josoder <j.soderstrand@gmail.com>
2017-10-04 20:50:23 +0200

    completed -test

--- md5_file.c
+++ md5_file.c
@@ -10,25 +10,68 @@ const char *STRIP = "-string";
 const char *MARK = "md5_checksum:"; 
 const char *END_MARK = "md5_checksum end";
 
+void create_digest(char *buffer,size_t size_of_buffer, char *digest);
 bool has_digest(FILE *fptr);
-
+size_t calc_file_size(FILE *f);
+bool is_equal(char *buf1, char *buf2);
 
 /**
  * Check if the checksum at the end of the file is valid
  */
 int checksum_is_valid(char *filename){
+   FILE *f = fopen(filename, "rb");
+   
+   BYTE md5_buffer[MD5_BLOCK_SIZE];
+   fseek(f, -MD5_BLOCK_SIZE, SEEK_END);
+   
+   fread(md5_buffer, 1, MD5_BLOCK_SIZE, f); 
     
+   puts("checksum atached to file is:");
+   for(int i=0; i<MD5_BLOCK_SIZE; i++){
+       printf("%02X", (BYTE) md5_buffer[i]);
+   }
+   
+   int limit = (strlen(MARK)+MD5_BLOCK_SIZE);
+   int b_size = calc_file_size(f) - limit; 
+   BYTE f_buffer[b_size];
+   
+   BYTE md5_check[MD5_BLOCK_SIZE];
+   fread(f_buffer, 1, b_size, f);
+   create_digest(f_buffer, b_size, md5_check);
+    
+   puts("recalculated files checksum is:");
+   for(int i=0; i<MD5_BLOCK_SIZE; i++){
+       printf("%02X", (BYTE) md5_check[i]);
+   }
+
+   puts("");
+
+   
+   fclose(f);
+
+   if(!is_equal) {
+       puts("the checksum does not match up");
+       return 1;
+   }
+   puts("checksum atached is correct");
+   return 0; 
 } 
 
+bool is_equal(char *buf1, char *buf2){
+    for(int i=0; i<MD5_BLOCK_SIZE; i++){
+        if(buf1[i]!=buf2[i]) return false;
+    }
+    return true; 
+}
 
 /**
  *Calculate the size
  */
 size_t calc_file_size(FILE *f){
-	fseek(f, 0L, SEEK_END);
-	size_t sz = ftell(f); 
-	rewind(f); 
-	return sz;
+    fseek(f, 0L, SEEK_END);
+    size_t sz = ftell(f); 
+    rewind(f); 
+    return sz;
 }
 
 /**
@@ -52,30 +95,32 @@ int print_mark_to_file(char *filename) {
  *Add checksum to file
  */ 
 int add_checksum_to_file(unsigned char *digest, char *filename){
-	FILE *fptr = fopen(filename, "ab");
-
-	if(fptr == NULL) {
-		return 1;
-	}
     // add marker before checksum
-	if (print_mark_to_file(filename)!=0){
+    if (print_mark_to_file(filename)!=0){
         return 1; 
     }
     
-    fwrite(digest, sizeof(digest[0]), sizeof(digest), fptr);
+    FILE *fptr = fopen(filename, "ab");
+
+    if(fptr == NULL) {
+        return 1;
+    }
+
+
+    fwrite(digest, 1, MD5_BLOCK_SIZE, fptr);
     fclose(fptr);
-	return 0; 
+    return 0; 
 }
 
 /**
  * Print description on how to use this app.
  */
 void print_usage(){
-	puts("wrong nr of args"); 
-	puts("usage:");
-	puts("md5 -add <filename>");
-	puts("md5 -test <filename>");
-	puts("md5 -strip <filename>");
+    puts("wrong nr of args"); 
+    puts("usage:");
+    puts("md5 -add <filename>");
+    puts("md5 -test <filename>");
+    puts("md5 -strip <filename>");
 }
 
 /**
@@ -83,26 +128,29 @@ void print_usage(){
  */ 
 bool has_digest(FILE *fptr){
     unsigned char buff[strlen(MARK)]; 
-    
     if(calc_file_size(fptr)<sizeof(buff) + MD5_BLOCK_SIZE){
-        return false;
+    return false;
     }
-    
-    // Place cursor at where the mark would have been placed
-    fseek(fptr, calc_file_size(fptr) - (sizeof(buff) + MD5_BLOCK_SIZE), SEEK_SET); 
-    fread(buff, sizeof(buff), sizeof(buff[0]), fptr); 
 
-    for(int i=0; i<strlen(MARK); i++){
-        if(buff[i]!=MARK[i]) return false;
-    }
+    // Place cursor at where the mark would have been placed
+    fseek(fptr, -(sizeof(buff)+MD5_BLOCK_SIZE), SEEK_END); 
+    fread(buff, 1, sizeof(buff), fptr); 
+    
+    // check if the read bytes contains the mark
+    if(strcmp(buff, MARK)!=0) return false;
 
     return true;
-}
+}   
 
-void create_digest(char *buffer, char *digest,  MD5_CTX ctx){
-    md5_update(&ctx, buffer, sizeof(buffer)); 
-	
-	md5_final(&ctx, digest);	
+void create_digest(char *buffer,size_t size_of_buffer, char *digest){
+    MD5_CTX ctx;
+    md5_init(&ctx);
+    
+    md5_update(&ctx, buffer, size_of_buffer); 
+   
+    md5_final(&ctx, digest);
+  
+    puts("");
 }
 
 /**
@@ -112,49 +160,50 @@ void create_digest(char *buffer, char *digest,  MD5_CTX ctx){
  * md5 -strip <filename>
  */
 int main(int argc, char *argv[]){
-	if(argc<3 && (strcmp(argv[1], ADD)!=0 || strcmp(argv[1], TEST)!=0 || strcmp(argv[1], STRIP)!=0)){
-		print_usage();
-		return 1;
-	}
+    if(argc<3 && (strcmp(argv[1], ADD)!=0 || strcmp(argv[1], TEST)!=0 || strcmp(argv[1], STRIP)!=0)){
+        print_usage();
+        return 1;
+    }
 
-	unsigned char digest[MD5_BLOCK_SIZE];    
-	MD5_CTX ctx;
-	
-	FILE *fptr = fopen("file", "rb"); 
-	if(fptr==NULL){
-		puts("error opening file");
-		return 1;
-	}
+    BYTE digest[MD5_BLOCK_SIZE];    
+    FILE *fptr = fopen("file", "rb"); 
+    if(fptr==NULL){
+        puts("error opening file");
+        return 1;
+    }
 
     size_t file_size = calc_file_size(fptr);
 
-	unsigned char buffer[file_size];
+    unsigned char buffer[file_size];
 
-	// read file into buffer
-	fread(buffer, file_size, 1, fptr);
-    rewind(fptr);
-
+    // read file into buffer
+    fread(buffer, file_size, 1, fptr);
+   
     bool contains_digest = has_digest(fptr);
     fclose(fptr);
 
     
     if(strcmp(argv[1], ADD)==0){
         if(contains_digest){
-                puts("file already contains checksum");
-                    return 1;
-                }
-        create_digest(digest, buffer, ctx);
-        add_checksum_to_file(digest, argv[2]);    
-    }   
-    else if(strcmp(argv[1], TEST)==0){
-        if(!contains_digest){
-            puts("file does not contain any checksum");
-            return 1; 
+            puts("file already contains checksum");
+            return 1;
         }
         
+    create_digest(buffer, file_size,digest);
+        add_checksum_to_file(digest, argv[2]);    
+    }  
+    else if(strcmp(argv[1], TEST)==0){
+            
+            if(!contains_digest){
+             puts("file does not contain any checksum");
+                return 1; 
+            }
+         
+            checksum_is_valid(argv[2]);
+
     }  
 
-	return 0;
+    return 0;
 }
 
 
